cf7f7895bdcc6b6b8c5087d3684abfe067e0d64b
Samar Pratap Singh <35553535+Samar-080301@users.noreply.github.com>
2020-10-01 23:59:19 +0530

    Updates the code.
    Added basic operations

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,2 +1,10 @@
-a = input()
-b = input()
+a = float(input())
+b = float(input())
+def calci(a,b):
+  add = a+b
+  sub = a-b
+  mul = a*b
+  div = a/b
+  mod = a%b
+  print(add,sub,mul,div,mod)
+calci(a,b)  
